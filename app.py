5faae8db4cad32523d6486718a5752af83acceb1
Abhay Raj <71041500+GithubDev939@users.noreply.github.com>
2022-09-10 20:04:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ def proposal_form():
 
 app = Flask(__name__)
 @app.route('/update_server', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('https://github.com/GithubDev939/humans-need-code-site')
-            origin = repo.remotes.origin
-            origin.pull()
-            return 'Updated PythonAnywhere successfully', 200
-        else:
-            return 'Wrong event type', 400
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/GithubDev939/humans-need-code-site')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
