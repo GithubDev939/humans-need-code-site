f175936a24540c284016d84a5b10e21a59e7814a
Abhay Raj <71041500+GithubDev939@users.noreply.github.com>
2022-09-10 20:02:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route('/update_server', methods=['POST'])
     def webhook():
         if request.method == 'POST':
-            repo = git.Repo('path/to/git_repo')
+            repo = git.Repo('https://github.com/GithubDev939/humans-need-code-site')
             origin = repo.remotes.origin
             origin.pull()
             return 'Updated PythonAnywhere successfully', 200
